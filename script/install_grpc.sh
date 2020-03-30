c929c50eeb7817706ec664e6588db5a20928fc89
Masahiro Ono <masahiro.o@gmail.com>
2020-03-30 17:19:28 +0900

    delete symbolic link for libgrpc++.so

--- script/install_grpc.sh
+++ script/install_grpc.sh
@@ -7,9 +7,6 @@ git submodule update --init
 make
 make install
 
-# I should do, but I want to know correct solution
-ln -s /usr/local/lib/libgrpc++.so /usr/local/lib/libgrpc++.so.1
-
 # Cleanup
 cd ~
 rm -rf /tmp/grpc
