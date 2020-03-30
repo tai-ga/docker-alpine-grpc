46da1d7f55ac1fe0c114b9ebbef5a3920c2f953b
Masahiro Ono <masahiro.o@gmail.com>
2020-03-30 17:20:43 +0900

    Add packages

--- script/before.sh
+++ script/before.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apk add --no-cache libstdc++
-apk add --no-cache --update --virtual .build-dependencies git make g++ unzip autoconf automake libtool file openssl curl
+apk add --no-cache --update --virtual .build-dependencies git make g++ unzip autoconf automake libtool file openssl curl openssl-dev linux-headers
 
